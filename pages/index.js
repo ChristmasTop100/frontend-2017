65f036494308e4bf914e6a7defe11d89c5565f3e
Yaron Tal <yaron.tal@oneshoe.nl>
2017-09-17 21:51:12 +0200

    Simple fetcher of songs

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,27 @@
-export default () => <div>Welcome to the christmas top 100!</div>
+import fetch from 'isomorphic-unfetch'
+
+const Index = (props) => (
+  <div>
+    <h1>Christmas top 100</h1>
+    <ul>
+      {props.songs.map(function (song) {
+        return (
+          <li key={song.id}>
+            {song.title} - {song.artist}
+          </li>
+        )
+      })}
+    </ul>
+  </div>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://back.christmastop100.nl/api/songs')
+  const data = await res.json()
+
+  return {
+    songs: data
+  }
+}
+
+export default Index
